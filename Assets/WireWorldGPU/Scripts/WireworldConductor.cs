e23aadf1bf2f0c91d133a4416fe205ffb0e67772
Andre Hamman <hammanandre@gmail.com>
2021-01-20 10:49:54 +0200

    Minor Refactor + comments
    Cleared up the bug in the compute shader that turned wires states into empty states
    
    Quality of life changes
    
    Co-Authored-By: MachidoTheKid <43815694+MachidoTheKid@users.noreply.github.com>

--- Assets/WireWorldGPU/Scripts/WireworldConductor.cs
+++ Assets/WireWorldGPU/Scripts/WireworldConductor.cs
@@ -3,122 +3,116 @@ using System.Collections.Generic;
 using UnityEngine.Rendering;
 using UnityEngine;
 using System.IO;
+using System;
 
 public class WireworldConductor : MonoBehaviour
-
 {
+    const int N = 8;
 
-    const int N=7;
-
-    public ComputeBuffer WireWorldBufferPrevious, WireWorldBufferNext;
+    public ComputeBuffer WireWorldStatePrevious, WireWorldStateNext;
+    int[] TempBuffer = new int[N * N];
 
     public ComputeShader WireWorldRules;
 
-    
-
-
-    void DebugBufferWrite(ComputeBuffer buffer, string filename = @"C:\Users\Nathan Duker\Documents\Debug.txt")
+    void Start()
     {
+        WireWorldStatePrevious = new ComputeBuffer(N * N, sizeof(int));
+        WireWorldStateNext = new ComputeBuffer(N * N, sizeof(int));
 
+        //Preload the Start State with some Data
+        WireWorldStatePrevious.SetData(CreateClock8x8WithClock());
+
+        Shader.SetGlobalInt("_Size", N);
+        WireWorldRules.SetInt("_Size", N);
+    }
+
+    void AutomataStep()
+    {
+        //Previous state = Next State
+        WireWorldStateNext.GetData(TempBuffer);
+        WireWorldStatePrevious.SetData(TempBuffer);
+
+        //Load & Trigger the Compute shader
+        WireWorldRules.SetBuffer(0, "_Input", WireWorldStatePrevious);
+        WireWorldRules.SetBuffer(0, "_Result", WireWorldStateNext);
+        WireWorldRules.Dispatch(0, N/8, N/8, 1);
+
+        //Set the Render shader's Content to the Next State
+        Shader.SetGlobalBuffer("_Cells", WireWorldStateNext);
+    }
+
+    int AutomataStepDelay = 0;
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0)){
+        //    runAutomata();
+        //}
+
+        //if (Input.GetMouseButton(0))
+        {
+            if (AutomataStepDelay >60)
+            {
+                AutomataStep();
+                AutomataStepDelay = 0;
+            }
+            AutomataStepDelay++;
+        }
+    }
+
+    void OnDestroy()
+    {
+        WireWorldStatePrevious.Release();
+        WireWorldStateNext.Release();
+    }
+
+    void DebugBufferWrite(ComputeBuffer buffer, string filename = @"D:\Debug.txt")
+    {
         int[] Debugbuffer = new int[N * N];
         buffer.GetData(Debugbuffer);
         using (var outf = new StreamWriter(filename))
             for (int i = 0; i < Debugbuffer.Length; i++)
                 outf.WriteLine(Debugbuffer[i]);
-
     }
 
-    void OnDestroy()
+    int[] CreateClock7x7WithClock()
     {
+        int[,] array2D = new int[7, 7] {{ 0, 0, 0, 0, 0, 0, 0 },
+                                        { 0, 0, 0, 0, 0, 0, 0 },
+                                        { 0, 0, 1, 1, 1, 0, 0 },
+                                        { 0, 1, 0, 0, 0, 1, 0 },
+                                        { 0, 0, 3, 2, 1, 0, 0 },
+                                        { 0, 0, 0, 0, 0, 0, 0 },
+                                        { 0, 0, 0, 0, 0, 0, 0 }};
 
-        WireWorldBufferPrevious.Release();
-        WireWorldBufferNext.Release();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-        WireWorldBufferPrevious = new ComputeBuffer(N * N, sizeof(int));
-        WireWorldBufferNext = new ComputeBuffer(N * N, sizeof(int));
-        //int[] tempArr = new int[N * N];
-
-
-
-
-        int[,] array2D = new int[7, 7] { { 0, 1, 1, 1, 1, 0, 0 },
-                                             { 1, 0, 0, 0, 0, 1, 0 },
-                                             { 0, 1, 3, 2, 1, 0, 0 },
-                                             { 0, 0, 0, 0, 0, 0, 0 },
-                                             { 0, 0, 0, 0, 0, 0, 0 },
-                                             { 0, 0, 0, 0, 0, 0, 0 },
-                                             { 0, 0, 0, 0, 0, 0, 0 } };
         int[] array1d = new int[7 * 7];
-
-        for (int x = 0; x < 6; x++)
+        for (int x = 0; x < 7; x++)
         {
-            for (int y = 0; y < 6; y++)
+            for (int y = 0; y < 7; y++)
             {
                 array1d[x * 7 + y] = array2D[x, y];
-
             }
-
         }
+        return array1d;
+    }
+    int[] CreateClock8x8WithClock()
+    {
+        int[,] array2D = new int[8, 8] {{ 0, 0, 0, 0, 0, 0, 0, 0 },
+                                        { 0, 0, 0, 0, 0, 0, 0, 0 },
+                                        { 0, 0, 1, 1, 1, 1, 0, 0 },
+                                        { 0, 1, 0, 0, 0, 0, 1, 0 },
+                                        { 0, 1, 0, 0, 0, 0, 1, 0 },
+                                        { 0, 0, 3, 2, 1, 1, 0, 0 },
+                                        { 0, 0, 0, 0, 0, 0, 0, 0 },
+                                        { 0, 0, 0, 0, 0, 0, 0, 0 }};
 
-
-
-
-        /*for(int i = 0; i<(N*N)-1; i++)
+        int[] array1d = new int[8 * 8];
+        for (int x = 0; x < 8; x++)
         {
-            tempArr[i] = Mathf.RoundToInt(i % 2);
-
-
-        }*/
-
-        WireWorldBufferPrevious.SetData(array1d);
-        DebugBufferWrite(WireWorldBufferPrevious);
-        Shader.SetGlobalBuffer("_Buffer", WireWorldBufferPrevious);
-        WireWorldRules.SetBuffer(0,"_Input", WireWorldBufferPrevious);     
-        WireWorldRules.SetBuffer(0,"_Result", WireWorldBufferNext);
-
-        WireWorldRules.Dispatch(0, 7, 7, 1);
-
-        DebugBufferWrite(WireWorldBufferNext, @"C:\Users\Nathan Duker\Documents\Demo2.txt");
-
-
-
-
-    }
-
-    // Update is called once per frame\
-    int framecount = 0;
-    void runAutomata()
-    {
-      
-        if (framecount > 20)
-        {            
-            int[] bufferarray = new int[N * N];
-            WireWorldBufferNext.GetData(bufferarray);
-            WireWorldBufferPrevious.SetData(bufferarray);
-            WireWorldRules.SetBuffer(0, "_Input", WireWorldBufferPrevious);
-            WireWorldRules.SetBuffer(0, "_Result", WireWorldBufferNext);
-            WireWorldRules.Dispatch(0, 7, 7, 1);
-            Shader.SetGlobalBuffer("_Buffer", WireWorldBufferNext);
-            framecount = 0;
+            for (int y = 0; y < 8; y++)
+            {
+                array1d[x * 8 + y] = array2D[x, y];
+            }
         }
-        framecount++;
-        Debug.Log(framecount);
-    }
-  
-    void Update()
-    {
-        if (Input.GetMouseButton(0)){
-
-            //Debug.Log("Pressed primary button.");
-            runAutomata();
-
-
-    }
-
-
+        return array1d;
     }
 }
--- Assets/WireWorldGPU/Shaders/BufferRender.shader
+++ Assets/WireWorldGPU/Shaders/BufferRender.shader
@@ -2,13 +2,10 @@
 {
     Properties
     {
-        _MainTex ("Texture", 2D) = "white" {}
+		[HideInInspector]_MainTex("Texture", 2D) = "white" {}
         _LineColor ("Line Color", Color) = (1,1,1,1)
         _LineSize("Line Size", Range(0,1)) = 0.15
         [IntRange] _GridSize("Grid Size", Range(1,100)) = 10
-
-
-
     }
     SubShader
     {
@@ -20,8 +17,6 @@
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
-            // make fog work
-            #pragma multi_compile_fog
 
             #include "UnityCG.cginc"
             #define S_empty float3(0,0,0)
@@ -31,16 +26,13 @@
 
             sampler2D _MainTex;
 
-
             //Grid Setup
             float4 _LineColor;
             float _GridSize;
 		    float _LineSize;
-            //States Setup
 
-
-           uniform StructuredBuffer<int> _Buffer;
-
+			uniform StructuredBuffer<int> _Cells;
+			uniform int _Size;
 
             struct appdata
             {
@@ -51,7 +43,6 @@
             struct v2f
             {
                 float2 uv : TEXCOORD0;
-                UNITY_FOG_COORDS(1)
                 float4 vertex : SV_POSITION;
             };
 
@@ -62,84 +53,60 @@
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
                 o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-                UNITY_TRANSFER_FOG(o,o.vertex);
                 return o;
             }
 
-
             uint cellcoordinatetoindex(float2 coord){
-
-                return uint (coord.x*7+coord.y);
-
-              
+                return uint (coord.x*_Size +coord.y);
             }
 
             float4 StateToColour(int state){
 
                 float4 color;
-                
                 if (round(state) == 0)
 			    {
 				    color = float4(S_empty, 1.0);
 			    }
-
                 if (round(state) == 1)
 			    {
 				    color = float4(S_wire, 1.0);
 			    }
-
                 if (round(state) == 2)
 			    {
 				    color = float4(S_head, 1.0);
 			    }
-
                 if (round(state) == 3)
 			    {
 				    color = float4(S_tail, 1.0);
 			    }
-
                 return color;
             }
 
             float3 grid(float2 uv){
 
-
 			    float gsize = floor(_GridSize);
-
 			    gsize += _LineSize;
 
+				// UV to Coordinate
 			    float2 id;
-
 			    id.x = floor(uv.x/(1.0/gsize));
 			    id.y = floor(uv.y/(1.0/gsize));
 
-			    float4 color = StateToColour(_Buffer[cellcoordinatetoindex(id)]);
+				//Coordinate to Index, Value from Buffer by Index, Value to Colour 
+			    float4 color = StateToColour(_Cells[cellcoordinatetoindex(id)]);
 
-
-
+				//Check if UV is a line
 			    if (frac(uv.x*gsize) <= _LineSize || frac(uv.y*gsize) <= _LineSize)
 			    {
 				    color = _LineColor;
 			    }
 
- 
-
-                //
-
                 return color;
-
-
             }
 
             fixed4 frag (v2f i) : SV_Target
             {
-                // sample the texture
-                fixed4 col = tex2D(_MainTex, i.uv); 
-                col=float4 (grid(i.uv),1);
-                // apply fog
-                UNITY_APPLY_FOG(i.fogCoord, col);
-                //return float4 (i.uv, 0,0);
-                return col;
+                return float4(grid(i.uv), 1);
             }
             ENDCG
         }
